62f840237b9c5094dda71c70ef0a59caff019f24
Patty Case <beadypatty@yahoo.com>
2018-10-13 14:18:57 -0700

    Initial Commit

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Patty Case on 10/13/18.
+//  Copyright © 2018 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
